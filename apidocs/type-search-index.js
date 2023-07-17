3633551fcd4b9a1355e1ec32a797a748c519dfcc
fuinyroot <fuinyroot@users.noreply.github.com>
2023-07-17 15:55:47 +0000

    Site checkin for project mavendb

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.fuiny.mavendb","l":"Artifactinfo"},{"p":"com.fuiny.mavendb","l":"Main"},{"p":"com.fuiny.mavendb","l":"MvnScanner"}];updateSearchResults();
